72b235c2bb7614d1c2adcfe32af203683e23ad6d
enzocxt <enzocxt@gmail.com>
2016-08-08 16:31:45 +0200

    modify config file

--- wrapper.py
+++ wrapper.py
@@ -23,12 +23,7 @@ def fpMining(inputs):
     elif inputs['type'] == 'sequence':
         method = prefixSpan(inputs)
     elif inputs['type'] == 'itemset':
-        if 'dominance' in inputs:
-            eclat_inputs = dict()
-            for key in inputs:
-                eclat_inputs[key] = inputs[key]
-            eclat_inputs['dominance'] = ''
-        method = eclat(eclat_inputs)
+        method = eclat(inputs)
     else:
         print 'Does not support "type == %s"!' % inputs['type']
         sys.exit(2)
@@ -36,54 +31,49 @@ def fpMining(inputs):
     output = method.mining()
     patterns = method.parser(output)
     return patterns
-
-
-def test(inputs):
-    method = Mining(inputs)
-    if inputs['type'] == 'graph':
-        method = gSpan(inputs)
-    elif inputs['type'] == 'sequence':
-        method = prefixSpan(inputs)
-    elif inputs['type'] == 'itemset':
-        method = eclat(inputs)
-    else:
-        print 'Does not support "type == %s"!' % inputs['type']
-        sys.exit(2)
-
-    #t1 = method.mining()
-    result = method.mining()
-    patterns = method.parser(result)
-    return patterns
-    #closedPatterns, t2 = method.closedMining()
-    #return patterns, t1, closedPatterns, t2
 
 
 @logger
 def fpMining_pure(inputs):
     if inputs['type'] == 'graph':
+        inputs['data'] = 'data/gSpan/' + inputs['data']
+        inputs['output'] = 'output/gSpan/' + inputs['output']
         method = gSpan(inputs)
     elif inputs['type'] == 'sequence':
+        inputs['data'] = 'data/prefixSpan/' + inputs['data']
+        inputs['output'] = 'output/prefixSpan/' + inputs['output']
         method = prefixSpan(inputs)
     elif inputs['type'] == 'itemset':
+        inputs['data'] = 'data/eclat/' + inputs['data']
+        inputs['output'] = 'output/eclat/' + inputs['output']
         method = eclat(inputs)
     else:
         print 'Does not support "type == %s"!' % inputs['type']
         sys.exit(2)
 
     output = method.mining()
-    print 'Pure gSpan output:\n%s' % output
     patterns = method.parser(output)
-    print 'Number of frequent sequences: %s' % len(patterns)
+    print "\n*************************************"
+    print 'Number of frequent patterns with constraints (pure exec): %s' % len(patterns)
     return patterns
 
 
 @logger
 def fpMining_IDP(inputs):
     if inputs['type'] == 'graph':
+        if 'data/' not in inputs['data']:
+            inputs['data'] = 'data/gSpan/' + inputs['data']
+            inputs['output'] = 'output/gSpan/' + inputs['output']
         method = gSpan(inputs)
     elif inputs['type'] == 'sequence':
+        if 'data/' not in inputs['data']:
+            inputs['data'] = 'data/prefixSpan/' + inputs['data']
+            inputs['output'] = 'output/prefixSpan/' + inputs['output']
         method = prefixSpan(inputs)
     elif inputs['type'] == 'itemset':
+        if 'data/' not in inputs['data']:
+            inputs['data'] = 'data/eclat/' + inputs['data']
+            inputs['output'] = 'output/eclat/' + inputs['output']
         if 'dominance' in inputs:
             eclat_inputs = dict()
             for key in inputs:
@@ -104,17 +94,20 @@ def fpMining_IDP(inputs):
 
     # closed pattern mining by generated IDP code
     idp_gen = IDPGenerator(params)
-    path, filename = os.path.split(params['datafile'])
+    path, filename = os.path.split(params['data'])
     idp_program_name = '{0}_{1}_{2}'.format(params['dominance'], params['type'], filename.split('.')[0])
+    # generate idp code for finding pattern with constraints
     idp_gen.gen_IDP_code(patterns, idp_program_name)
-    sys.exit(2)
-    #idp_output = idp_gen.run_IDP(idp_program_name)
+    # run generated idp code
+    idp_output = idp_gen.run_IDP(idp_program_name)
+    # parser the idp output
     indices = set(idp_gen.parser_from_stdout(idp_output))
 
     closed_patterns = []
     for p in patterns:
         if p.id in indices:
             closed_patterns.append(p)
+    print "\n*************************************"
     print "\nNumber of {0} frequent patterns: {1}\n".format(params['dominance'], len(closed_patterns))
 
     return closed_patterns
@@ -130,18 +123,19 @@ if __name__ == "__main__":
     params = {}     # Dict to store input parameters
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hc:i:o:', ['help=', 'config=', 'infile=', 'outfile='])
+        # opts, args = getopt.getopt(sys.argv[1:], 'hc:i:o:', ['help=', 'config=', 'infile=', 'outfile='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hc:', ['help=', 'config='])
     except getopt.GetoptError:
-        print('wrapper.py -c <configfile> -i <inputfile> -o <outputfile>')
+        print('wrapper.py -c <configfile>\nSet input data and output file in config file')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             print 'wrapper.py -c <configfile> -i <inputfile> -o <outputfile>'
             sys.exit(2)
-        elif opt in ('-i', '--infile'):
-            params['datafile'] = arg
-        elif opt in ('-o', '--outfile'):
-            params['output'] = arg
+        #elif opt in ('-i', '--infile'):
+        #    params['datafile'] = arg
+        #elif opt in ('-o', '--outfile'):
+        #    params['output'] = arg
         elif opt in ('-c', '--config'):
             config_file = arg
 
