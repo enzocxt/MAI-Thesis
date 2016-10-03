3e713c279ea7c8aefb2854f4e950a0d344b46326
enzocxt <enzocxt@gmail.com>
2016-10-03 21:50:22 +0200

    fix sequence multiple idp

--- solver/groupIDP.py
+++ solver/groupIDP.py
@@ -1,9 +1,9 @@
 __author__ = 'enzo'
 
 from multiprocessing import Process, Pool, Lock
-import os, time, random
+import os, sys, time, random
 
-
+# with lock
 def groupIDP(mapping, idp_gen, idp_program_name):
     idp_gen.gen_IDP_code_group(mapping, idp_program_name)
     lock.acquire()
@@ -46,8 +46,6 @@ def async_mapping(mapping_groups, idp_gen, idp_program_base):
 def groupIDP_withoutLock(mapping, idp_gen, idp_program_name):
     idp_gen.gen_IDP_code_group(mapping, idp_program_name)
     output = idp_gen.run_IDP(idp_program_name)
-    print output
-    print "************************"
 
     nonclosed_indices = set()
     lines = output.split('\n')
@@ -73,9 +71,43 @@ def async_mapping_withoutLock(mapping_groups, idp_gen, idp_program_base):
     return output
 
 
+# put idp output into separate output file
+def groupIDP_separate(mapping, idp_gen, idp_program_name, output_file):
+    sys.stdout = open('{out}_{pid}.out'.format(out=output_file, pid=str(os.getpid())), 'w')
+
+    idp_gen.gen_IDP_code_group(mapping, idp_program_name)
+    output = idp_gen.run_IDP(idp_program_name)
+    '''
+    fout = open('{0}_{1}.txt'.format(output_file, os.getpid()), 'w')
+    fout.write(output)
+    fout.close()
+    '''
+
+def async_mapping_separate(mapping_groups, idp_gen, idp_program_base):
+    pool = Pool()
+    results = []
+    output = ''
+
+    for i, mapping in enumerate(mapping_groups):
+        results.append(pool.apply_async(groupIDP_separate, args=(mapping, idp_gen, '{0}_{1}'.format(idp_program_base, str(i)), 'output/idp/{0}'.format(str(i)))))
+    pool.close()
+    pool.join()
+
+    for file in os.listdir('output/idp/'):
+        fin = open('output/idp/'+file, 'r')
+        output += fin.read() + '\n**************************************************\n'
+        fin.close()
+
+    for file in os.listdir('output/idp/'):
+        os.remove('output/idp/' + file)
+
+    return output
+
 
 # For test
 def long_time_task(name):
+    sys.stdout = open(str(os.getpid()) + 'out', 'w')
+
     print 'Run task %s (%s)...' % (name, os.getpid())
     start = time.time()
     time.sleep(random.random() * 3)
