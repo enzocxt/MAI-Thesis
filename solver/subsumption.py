b5ea97c9985efaa69a9e1e7eecdcaa8152218d91
Sergey <sergey.paramonov@phystech.edu>
2016-10-04 19:43:38 +0200

    made first version of the subsumption tree

--- solver/subsumption.py
+++ solver/subsumption.py
@@ -1,30 +1,33 @@
-from solver.method import prefixSpan#for debugging           
+#from solver.method import prefixSpan#for debugging           
 from solver.data_structures import group_by_len, make_attribute_mapping, get_attribute_intersection
 from collections import defaultdict
 
-def main():
-  with open("output/prefix_sequence_user_pos.txt","r") as datafile:
-    data = datafile.read()
-  patterns = prefixSpan.parserSequence(data) 
-  create_subsumption_lattice(patterns)
- #p1 = patterns[100].get_attribute_array()
- #p2 = patterns[105].get_attribute_array()
- #p3 = patterns[102].get_attribute_array()
- #print(p1,p2,p3)
+# def main():
+#   with open("output/prefix_sequence_user_pos.txt","r") as datafile:
+#     data = datafile.read()
+#   patterns = prefixSpan.parserSequence(data) 
+#   subsumption_tree = create_subsumption_lattice(patterns)
+#   print(subsumption_tree)
 
 
 def create_subsumption_lattice(patterns):
-  subsumsed_by = defaultdict(set)
+  subsumption_tree = defaultdict(set)
   mapping_by_len = group_by_len(patterns)
   max_len = max(mapping_by_len.keys())
   attribute_mapping = make_attribute_mapping(patterns)
 
   for l in range(max_len-1): # maximal are not subsumed by anything
+    print('processing len', l)
     sequences_with_len_l = mapping_by_len[l]
     for seq in sequences_with_len_l:
       sequences_with_len_l_plus_1 = mapping_by_len[l+1]
       with_at_least_the_same_attributes = get_attribute_intersection(seq, attribute_mapping)
       candidates = sequences_with_len_l_plus_1.intersection(with_at_least_the_same_attributes)
+      for candidate in candidates:
+        if is_seq1_subsumed_by_seq2(seq.get_attributes(),candidate.get_attributes()):
+          subsumption_tree[candidate].add(seq)
+
+  return subsumption_tree
   
 # print mapping_by_len
 # print attribute_mapping
